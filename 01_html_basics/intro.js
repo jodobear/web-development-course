c7fa595428f2c0f0afa0d86da9edb31d3a2800b5
Anrymart <andrei.martynenko@phystech.edu>
2019-09-28 11:41:06 +0300

    Lessons 1, 2

---
+++ 01_html_basics/intro.js
@@ -0,0 +1,7 @@
+var total = sum(1, 2);
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(total);
---
+++ 02_css_and_javascript/02_js/index.js
@@ -0,0 +1,47 @@
+/*
+ * Variables: let (var), const. Naming convention.
+ *
+ * Data types: number, string, boolean, null, undefined, object. Typeof operator. Typeof null.
+ * Browser console.
+ *
+ * Operators: +, -, =, %, **, ++, --, +=. Bitwise operators. QUESTION.
+ *
+ * Alert, prompt, confirm.
+ * Task: implement a script which asks for a name and outputs it.
+ *
+ * Comparisons. Strict equality ===.
+ *
+ * Conditional operators: if, else, else if, ?.
+ * Task: implement sign function.
+ * Task: rewrite if into ?.
+ *
+ * Loops - while and for. Breaking and continuing the loop.
+ * Task: output even numbers in a loop.
+ * Task: calculate prime numbers below 1000 and print them.
+ *
+ * Functions. Function declaration. Local and outer variables, parameters. Functions as arguments.
+ * Returning a value.
+ * Assigning function to a variable.
+ *
+ * Objects
+ *
+ * Arrays
+ */
+
+
+let num = 1;
+const city = "Moscow";
+
+num = "text";
+
+// Data types
+
+let boolVar = true; // boolean
+let numberVar = 1; // number
+let undefinedVar; // undefined
+let nullVar = null; // null
+let stringVar = "text"; // string
+let objectVar = { // object
+    a: 1
+};
+
